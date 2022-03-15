416395ed91b6d646457239f19d7337371af4ed5d
Adem Mrizak <adem.mrizak@gmail.com>
2022-03-15 13:00:41 +0100

    Test controller + build config

--- src/main/java/com/esgi/App.java
+++ src/main/java/com/esgi/App.java
@@ -1,13 +1,12 @@
 package com.esgi;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Spring!");
+        SpringApplication.run(App.class, args);
     }
 }
---
+++ src/main/java/com/esgi/TestController.java
@@ -0,0 +1,13 @@
+package com.esgi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test hello world");
+    }
+}
