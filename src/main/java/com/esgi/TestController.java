9da43b60fcb9b1fece60bc332efd64756adb048c
Adem Mrizak <adem.mrizak@gmail.com>
2022-03-16 18:06:01 +0100

    Code running from a string test fix

--- src/main/java/com/esgi/TestController.java
+++ src/main/java/com/esgi/TestController.java
@@ -4,27 +4,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> test() throws IOException {
         String pythonScript = "print('Hello')\nprint('World')";
+        final long startTime = System.nanoTime();
+
         ProcessBuilder processBuilder = new ProcessBuilder();
+
+        final String FILE_NAME = "/home/ec2-user/python_src/test.py";
+        File targetFile = new File(FILE_NAME);
+        targetFile.delete();
+        targetFile.createNewFile();
+
+        FileOutputStream outputStream = new FileOutputStream(targetFile);
+        outputStream.write(pythonScript.getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+
 
         // -- Linux --
 
         // Run a shell command
 //        processBuilder.command("bash", "-c", "ls /home/mkyong/");
 
-        try {
-            new ProcessBuilder().command("echo \"" + pythonScript + "\" > /home/ec2-user/python_src/test.py").start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            new ProcessBuilder().command("echo \"" + pythonScript + "\" > /home/ec2-user/python_src/test.py").start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         // Run a shell script
         processBuilder.command("/home/ec2-user/run_python.sh");
 
@@ -57,6 +68,9 @@ public class TestController {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        return ResponseEntity.ok(output.toString());
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime) / 1000000;  //divide by 1000000 to get milliseconds.
+        return ResponseEntity.ok(output + " " + duration + "ms");
     }
 }
