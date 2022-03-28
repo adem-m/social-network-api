1f14e7fda7aeb4e68a2bf946da41d2ae824032f1
Adem Mrizak <adem.mrizak@gmail.com>
2022-03-28 12:15:39 +0200

    Fix timeout implementation

--- src/main/java/com/esgi/modules/code/domain/Script.java
+++ src/main/java/com/esgi/modules/code/domain/Script.java
@@ -1,18 +1,3 @@
 package com.esgi.modules.code.domain;
 
-public class Script {
-    private static final String SCRIPT_PREFIX = "timeout 10s ";
-    private final String command;
-
-    private Script(String command) {
-        this.command = command;
-    }
-
-    public static Script of(String command) {
-        return new Script(SCRIPT_PREFIX + command);
-    }
-
-    public String getCommand() {
-        return command;
-    }
-}
+public record Script(String command) {}
