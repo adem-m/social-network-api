cfbc32598554443ab1f1a6cefd6cea0bc2f7d3b4
Adem Mrizak <adem.mrizak@gmail.com>
2022-04-06 10:25:26 +0200

    Revert "Output line size fix"
    This reverts commit 9e28822686e01a370e0633d40075322dc5be10f6.

--- src/main/java/com/esgi/modules/code/application/RunCodeCommandHandler.java
+++ src/main/java/com/esgi/modules/code/application/RunCodeCommandHandler.java
@@ -55,16 +55,13 @@ final public class RunCodeCommandHandler implements CommandHandler<RunCode, Outp
         return Output.success(output.toString(), duration);
     }
 
-    private void appendLines(StringBuilder output, BufferedReader reader) {
+    private void appendLines(StringBuilder output, BufferedReader reader) throws IOException {
         int outputSize = 0;
         String line;
-        try {
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
-                outputSize++;
-                if (outputSize > MAX_OUTPUT_SIZE) return;
-            }
-        } catch (Throwable ignored) {
+        while ((line = reader.readLine()) != null) {
+            output.append(line).append("\n");
+            outputSize++;
+            if (outputSize > MAX_OUTPUT_SIZE) break;
         }
     }
 }
