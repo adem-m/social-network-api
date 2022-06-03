a513232599706ac30e3bbcecd39468d1ca27578e
Adem Mrizak <adem.mrizak@gmail.com>
2022-06-03 12:44:40 +0200

    Test fixes

--- src/test/java/com/esgi/authentication/LoginFlowTest.java
+++ src/test/java/com/esgi/authentication/LoginFlowTest.java
@@ -1,9 +1,11 @@
 package com.esgi.authentication;
 
-//import com.esgi.config.UserTestConfig;
 import com.esgi.fixtures.UserTestFixtures;
 import com.esgi.modules.authentication.exposition.LoginResponse;
-
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
@@ -14,14 +16,12 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import io.restassured.RestAssured;
-
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(
-    webEnvironment = WebEnvironment.RANDOM_PORT
+        webEnvironment = WebEnvironment.RANDOM_PORT
 )
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
@@ -32,22 +32,27 @@ public class LoginFlowTest {
     @BeforeEach
     public void setup() {
         RestAssured.port = port;
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
     }
 
     @Test
     public void should_return_jwt() {
-        UserTestFixtures.insertTestUser();
+        UserTestFixtures.insertTestUser(port);
 
-        var res = when()
-            .post("/login")
-            .then()
-            .statusCode(201)
-            .extract()
-            .body().jsonPath().getObject(".", LoginResponse.class);
-        
+        var res =
+                given()
+                        .port(port)
+                        .contentType(ContentType.JSON)
+                        .when()
+                        .post("/login")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .body().jsonPath().getObject(".", LoginResponse.class);
+
         assertThat(res.token()).isInstanceOf(String.class);
         assertThat(res.userId()).isInstanceOf(String.class);
 
-        UserTestFixtures.deleteUser(res.token());
+        UserTestFixtures.deleteUser(res.token(), port);
     }
 }
--- src/test/java/com/esgi/fixtures/UserTestFixtures.java
+++ src/test/java/com/esgi/fixtures/UserTestFixtures.java
@@ -2,26 +2,27 @@ package com.esgi.fixtures;
 
 import com.esgi.modules.user.application.CreateUser;
 import com.esgi.modules.user.domain.Email;
-
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
 
 public class UserTestFixtures {
-    public static Response insertTestUser() {
+    public static Response insertTestUser(int port) {
         final var request = new CreateUser("lastname", "firstname", new Email("test@example.com"), "azertyUIOP123$");
         return given()
-            .contentType(ContentType.JSON)
-            .body(request)
-            .when()
-            .post("/users");
+                .port(port)
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/users/register");
     }
 
-    public static Response deleteUser(String token) {
+    public static Response deleteUser(String token, int port) {
         return given()
-            .header("Authorization", "Bearer " + token)
-            .when()
-            .delete("/users");
+                .port(port)
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .delete("/users");
     }
 }
