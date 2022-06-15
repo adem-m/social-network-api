13b78efc0e4689f1505a5c18ff52f3c9091d934d
Adem Mrizak <adem.mrizak@gmail.com>
2022-06-15 13:53:38 +0200

    Make post creation transactional

--- src/main/java/com/esgi/modules/post/application/CreatePostCommandHandler.java
+++ src/main/java/com/esgi/modules/post/application/CreatePostCommandHandler.java
@@ -9,11 +9,12 @@ import com.esgi.modules.post.domain.Post;
 import com.esgi.modules.post.domain.PostId;
 import com.esgi.modules.post.domain.PostRepository;
 import com.esgi.modules.user.domain.UserId;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public final class CreatePostCommandHandler implements CommandHandler<CreatePost, PostId> {
+public class CreatePostCommandHandler implements CommandHandler<CreatePost, PostId> {
     private final PostRepository postRepository;
     private final EventDispatcher<Event> eventEventDispatcher;
     private final CommandBus commandBus;
@@ -24,6 +25,7 @@ public final class CreatePostCommandHandler implements CommandHandler<CreatePost
         this.commandBus = commandBus;
     }
 
+    @Transactional
     public PostId handle(CreatePost createPost) {
         final PostId postId = postRepository.nextIdentity();
         final UserId creatorId = new UserId(createPost.creatorId);
